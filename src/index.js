bd70374afeb3b9543f0d89637ba554a0a46b82fb
Andrey <andrey.sviontek@gmail.com>
2019-03-12 11:32:11 +0500

    feat: completed task 'love-triangle'

--- src/index.js
+++ src/index.js
@@ -4,4 +4,17 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
-};
+  let number = 0;  
+  for (let i = 0; i<preferences.length; i++) { 
+      let first = preferences[i] - 1;
+      if (first == i) continue;
+      let second = preferences[first] - 1;
+      if (first == second) continue;
+      let fird = preferences[second] - 1;
+      if (fird == second) continue;
+      if (fird == i) {
+          number++;
+      }
+   }
+   return Math.round(number) / 3;
+}
